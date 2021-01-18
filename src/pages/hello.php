f4ac215173fbcb2cad68e4065708629eb3ca905f
Christopher Steven Ciampoli <admin@sdpro.dev>
2021-01-17 23:28:46 -0800

    Works atm

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,3 +1,4 @@
-<?php
-$name = $request->get('name', 'World');
-$response->setContent(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+<!-- example.com/src/pages/hello.php -->
+<?php $name = $request->get('name', 'World') ?>
+
+Hello <?= htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
--- web/front.php
+++ web/front.php
@@ -8,16 +8,18 @@ $request = Request::createFromGlobals();
 $response = new Response();
 
 $map = [
-'/hello' => __DIR__ . '/hello.php',
-'/bye'   => __DIR__ . '/bye.php',
+'/hello' => '../src/pages/hello.php',
+'/bye'   => '../src/pages/bye.php',
 ];
 
 $path = $request->getPathInfo();
 if (isset($map[$path])) {
-require $map[$path];
+    ob_start();
+    include $map[$path];
+    $response->setContent(ob_get_clean());
 } else {
-$response->setStatusCode(404);
-$response->setContent('Not Found');
+    $response->setStatusCode(404);
+    $response->setContent('Not Found');
 }
 
 $response->send();
